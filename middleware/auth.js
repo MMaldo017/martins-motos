801c568fd992934b82e235cf39d43b02bec38aae
MMaldo017 <maldonado.f.martin@gmail.com>
2024-08-28 19:50:48 -0500

    Profile route/ Schema, create middleware, get current user profile, login route

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+//verrify the accounts 
+const jwt = require('jsonwebtoken')
+const config = require("config");
+
+module.exports = function(req, res, next) {
+    const token = req.header('x-auth-token');
+    if(!token) {
+        return res.status(401).json({ msg: 'No token, authorization is denied'});
+    }
+
+
+//verify token
+try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    req.user = decoded.user
+    next();
+}catch(err){
+    res.status(401).json({msg: 'Your token is not valid'});
+}
+}
--- routes/API/profile.js
+++ routes/API/profile.js
@@ -1,6 +1,6 @@
 const express = require ('express')
 const router = express.Router();
-
+const auth = require('../../middleware/auth');
 //@router  GET api/profile/me
 //@desc Test route
 //@access Private
