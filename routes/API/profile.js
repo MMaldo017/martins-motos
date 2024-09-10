40e309aad2c214804f9461bdc924e586e49d52aa
MMaldo017 <maldonado.f.martin@gmail.com>
2024-09-09 19:47:03 -0500

    add experience/delete experience

--- routes/API/profile.js
+++ routes/API/profile.js
@@ -158,7 +158,7 @@ router.delete('/', auth, async (req, res) => {
 
 //@ route PUT api/profile/experience
 //@Desc add profile experience
-//@Public
+//@Private
 
 router.put(
   '/experience',
@@ -219,6 +219,93 @@ router.put(
  
   
   )
+//@ route Delete api/profile/experience
+//@Desc delete profile experience
+//@Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+ 
+    // Get remove index
+    const removeIndex = profile.experience
+     .map(item => item.id)
+     .indexOf(req.params.exp_id);
+ 
+    if (removeIndex < 0) {
+      return res.status(404).json({ msg: 'Experience not found' });
+    }
+ 
+    // Splice out of array
+    profile.experience.splice(removeIndex, 1);
+ 
+    await profile.save();
+ 
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+
+  
+
+
+
+
+//@ route PUT api/profile/education
+//@Desc add profile education
+//@Private
+
+router.put('/education',
+  [
+    auth,
+    [
+      check('school', 'School name is required').not().isEmpty(),
+      check('degree', 'Degree is required').not().isEmpty(),
+      check('fieldofstudy', 'Field of study is required').not().isEmpty(),
+    ]
+
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+      return res.status(400).json({ errors: errors.array()});
+    };
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const otherEducation = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    }
+    try {
+      const profile = await Profile.findOne({user: req.user.id});
+      profile.education.unshift(otherEducation);
+      await profile.save(); 
+      res.json(profile);
+    }catch (err){
+      console.error(err.message);
+      res.status(500).send('Server Error')
+    }
+    
+
+  }
+);
+
+
 
 
 module.exports = router;
